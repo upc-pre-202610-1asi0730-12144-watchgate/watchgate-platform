7be7cb15e67bcdf757832ec01d5086ac92f3b6ed
AlonsoHiga <u202416276@upc.edu.pe>
2026-07-01 18:52:16 -0500

    feat(subscription-management): add resources files

---
+++ Watchgate.Locksight.Platform/SubscriptionManagement/Resources/SubscriptionManagementMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Watchgate.Locksight.Platform.SubscriptionManagement.Resources;
+
+/// <summary>
+/// Marker class used for strongly typed string localization in Subscription Management.
+/// </summary>
+public class SubscriptionManagementMessages
+{
+}
