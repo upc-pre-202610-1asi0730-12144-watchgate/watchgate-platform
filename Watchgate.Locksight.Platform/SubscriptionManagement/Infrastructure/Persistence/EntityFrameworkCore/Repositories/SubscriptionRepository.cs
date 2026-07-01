454231817bbc3bcaabb533bf6c16c996e6443ea4
AlonsoHiga <u202416276@upc.edu.pe>
2026-07-01 18:51:03 -0500

    feat(subscription-management): add infrastructure files

---
+++ Watchgate.Locksight.Platform/SubscriptionManagement/Infrastructure/Persistence/EntityFrameworkCore/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Watchgate.Locksight.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using Watchgate.Locksight.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+using Watchgate.Locksight.Platform.SubscriptionManagement.Domain.Model.Aggregates;
+using Watchgate.Locksight.Platform.SubscriptionManagement.Domain.Model.ValueObjects;
+using Watchgate.Locksight.Platform.SubscriptionManagement.Domain.Repositories;
+
+namespace Watchgate.Locksight.Platform.SubscriptionManagement.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+public class SubscriptionRepository(AppDbContext context) 
+    : BaseRepository<Subscription, SubscriptionId>(context), ISubscriptionRepository
+{
+    public async Task<Subscription?> FindByCompanyIdAsync(int companyId, CancellationToken cancellationToken = default)
+    {
+        return await Context.Set<Subscription>()
+            .FirstOrDefaultAsync(s => s.CompanyId == companyId, cancellationToken);
+    }
+}
